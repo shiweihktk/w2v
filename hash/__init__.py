9794463f7e19b41c0f78a85372109ab806b96e87
wanwei <754153230@qq.com>
2016-04-05 14:40:41 +0800

    重新上传。

--- hash/__init__.py
+++ hash/__init__.py
@@ -17,7 +17,7 @@ def creathash(str1):
     #hash1=hashlib.md5()
     #hash1.update(str)
     #return int(hash1.hexdigest(),16)
-#a='我爱打篮球。阿卡；啊打发了肯德基；啊；就；阿的江；啊打发；付款的叫法地方；阿洛菲啊付款；带上飞机啊；了受到法律是会计法律房间打扫房间阿三啊；来得及伐交到了；放假啊都浪费粮食的肌肤啊放假啊的肌肤；垃圾的身份了；啊就发了；打飞机啊两地分居阿三顶顶；啊煞风景啊算法。'
+#a='我爱打篮球。i like'
 #b='i like playing game.'
 #print(creathash(a))
 #print(creathash(b))
